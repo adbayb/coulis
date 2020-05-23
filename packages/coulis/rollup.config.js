feb74406459170bd4d24c5fabd6334ebf6f06c27
Ayoub Adib <adbayb@gmail.com>
2020-05-23 21:04:37 +0200

    chore: update development flows

--- packages/coulis/rollup.config.js
+++ packages/coulis/rollup.config.js
@@ -1,25 +1,32 @@
+import path from "path";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
+
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 export default {
 	input: pkg.source,
 	output: [
 		{
-			file: pkg.main,
+			dir: path.dirname(pkg.main),
+			entryFileNames: path.basename(pkg.main),
 			format: "cjs",
 		},
 		{
-			file: pkg.module,
+			dir: path.dirname(pkg.module),
+			entryFileNames: path.basename(pkg.module),
 			format: "es",
 		},
 	],
 	plugins: [
 		typescript(),
-		terser({
-			output: {
-				comments: false,
-			},
-		}),
+		...(!isDevelopment
+			? terser({
+					output: {
+						comments: false,
+					},
+			  })
+			: []),
 	],
 };

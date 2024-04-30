8b0556bd66def79c17ada4a1c8fabcaf73059e24
Ayoub Adib <adbayb@gmail.com>
2024-04-30 02:37:36 +0200

    feat: play with new styles api

--- libraries/coulis/src/modules/styles.ts
+++ libraries/coulis/src/modules/styles.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { SHORTHAND_PROPERTIES } from "../constants";
 import { SCOPES } from "../entities/scope";
 import { isObject, toDeclaration, toManyDeclaration } from "../helpers";
@@ -7,7 +8,10 @@ import type {
 	AtomicStyleObject,
 	ClassName,
 	GlobalStyleObject,
+	StyleObject,
 } from "../types";
+
+import { createCustomProperties } from "./properties";
 
 /**
  * Create a class name to apply a style rule to an element.
@@ -134,3 +138,123 @@ const createClassName = (groupingRule = "") => {
 		return classNames.join(" ");
 	};
 };
+
+const px = (value: number) => `${value}px`;
+
+const tokens = {
+	colors: {
+		black: "black",
+		blue: [
+			"rgb(241,244,248)",
+			"rgb(226,232,240)",
+			"rgb(201,212,227)",
+			"rgb(168,186,211)",
+			"rgb(119,146,185)",
+		],
+		transparent: "transparent",
+		white: "white",
+	},
+	fontSizes: [
+		px(12),
+		px(14),
+		px(16),
+		px(18),
+		px(20),
+		px(22),
+		px(24),
+		px(28),
+		px(30),
+	],
+	fontWeights: ["100", "400", "900"],
+	radii: [px(0), px(4), px(8), px(12), px(999)],
+} as const;
+
+const theme = createCustomProperties({
+	colors: {
+		neutralDark: tokens.colors.black,
+		neutralLight: tokens.colors.white,
+		neutralTransparent: tokens.colors.transparent,
+		surfacePrimary: tokens.colors.blue[4],
+		surfaceSecondary: tokens.colors.blue[2],
+	},
+	radii: {
+		full: tokens.radii[4],
+		large: tokens.radii[3],
+		medium: tokens.radii[2],
+		none: tokens.radii[0],
+		small: tokens.radii[1],
+	},
+	typographies: {
+		body: {
+			fontSize: tokens.fontSizes[2],
+			fontWeight: tokens.fontWeights[1],
+		},
+	},
+});
+
+const createNewStyles = <
+	const StyleProperties extends
+		| Partial<
+				Record<
+					keyof StyleObject,
+					Record<string, string> | unknown[] | true
+				>
+		  >
+		| StyleObject = StyleObject,
+	Shorthands extends Record<string, (keyof StyleProperties)[]> = {},
+>(contract: {
+	properties?: StyleProperties; // or conditions
+	shorthands?: Shorthands;
+	states: Record<string | "base", `@media ${string}`>;
+}) => {
+	console.log(contract);
+
+	type ProcessedProperties = StyleProperties extends StyleObject
+		? StyleObject
+		: {
+				[Key in keyof StyleProperties]: StyleProperties[Key] extends (infer Value)[]
+					? Value
+					: StyleProperties[Key] extends Record<
+								infer CustomPropertyValue,
+								string
+						  >
+						? CustomPropertyValue
+						: StyleProperties[Key] extends true
+							? Key extends keyof StyleObject
+								? StyleObject[Key]
+								: never
+							: never;
+			};
+
+	type ProcessedShorthands = Shorthands extends undefined
+		? {}
+		: {
+				[K in keyof Shorthands]: Shorthands[K] extends (infer Properties)[]
+					? Properties extends keyof ProcessedProperties
+						? ProcessedProperties[Properties]
+						: never
+					: never;
+			};
+
+	// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+	return {} as ProcessedProperties & ProcessedShorthands;
+};
+
+const test = createNewStyles({
+	properties: {
+		backgroundColor: theme.colors,
+		boxShadow: true,
+		color: theme.colors,
+		paddingLeft: [0, 1, 2],
+		paddingRight: [2, 3],
+	},
+	shorthands: {
+		paddingHorizontal: ["paddingLeft", "paddingRight"],
+		surface: ["color", "backgroundColor"],
+	},
+	states: {
+		base: "@media (min-width: 0px)",
+	},
+});
+
+console.log(test);
